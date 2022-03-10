e5e5ab00e83b3efa9cbe8c7462d7d0ef7e0470a0
Bombarder70 <holespato@gmail.com>
2022-03-10 13:30:39 +0100

    Functions

--- Assets/js/Functions.js
+++ Assets/js/Functions.js
@@ -250,4 +250,36 @@ class Functions {
     }
   }
 
+  addToUrl(param, data) {
+    var url = new URL(window.location.href);
+    url.searchParams.set(param, data);
+    window.history.pushState({}, '', url);
+  };
+
+  deleteFromUrl(paramToDelete) {
+    var url = new URL(window.location.href);
+    url.searchParams.delete(paramToDelete);
+    window.history.pushState({}, '', url);
+  };
+
+  getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? this.transformToAssocArray(prmstr) : {};
+  }
+
+  transformToAssocArray(prmstr) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+      var tmparr = prmarr[i].split("=");
+      params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+  }
+
+  getUrlParam(param) {
+    var params = this.getSearchParameters();
+    return params[param];
+  };
+
 }
