36de30dbcd4c5d703eb100f431162c2d7ea19af6
Bombarder70 <holespato@gmail.com>
2022-03-10 12:58:41 +0100

    getCustomerIdCart

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -36,6 +36,7 @@ namespace Core\Controllers {
       if ($page == "login") {
         if (!empty(self::getLogged())) {
           $page = "moj-ucet";
+          self::setCustomerId();
         }
       } else if($page == "moj-ucet") {
         if (empty(self::getLogged())) {
@@ -45,20 +46,85 @@ namespace Core\Controllers {
     }
 
     public static function getCustomerUid() {
+      return isset($_COOKIE["customer_uid"]) ? $_COOKIE["customer_uid"] : "";
+    }
+
+    public static function getCustomerIdCart() {
+      $customerUid = self::getCustomerUid();
+
+      if ($customerUid != "") {
+        global $db;
+
+        $customers = $db->dbSelect(
+          "customers_uids",
+          [
+            "where" => [
+              "uid" => $customerUid
+            ]
+          ]
+        );
+
+        $carts = $db->dbSelect(
+          "carts",
+          [
+            "where" => [
+              "id_customer_uid" => reset($customers)['id']
+            ]
+          ]
+        );
+
+        return reset($carts)['id'];
+      }
+    }
+
+    public static function createCustomerUid() {
       global $db;
 
       if (empty($_COOKIE["customer_uid"])) {
-        $_COOKIE["customer_uid"] = \Core\Bice::getuid();
+        $uid = \Core\Bice::getuid();
 
-        $db->insert_array([
+        setcookie(
+          "customer_uid", 
+          $uid, 
+          time() + (3600 * 240), "/"
+        );
+
+        $idCustomerUid = $db->insert_array([
           "table" => "customers_uids",
           "table_data" => [
-            "uid" => $_COOKIE["customer_uid"]
+            "uid" => $uid
+          ]
+        ]);
+
+        $db->insert_array([
+          "table" => "carts",
+          "table_data" => [
+            "id_customer_uid" => $idCustomerUid
           ]
         ]);
       }
+    }
 
-      return $_COOKIE["customer_uid"];
+    public static function setCustomerId() {
+      global $db;
+
+      $customer = $db->dbSelect(
+        "customers_uids",
+        [
+          "where" => [
+            "id_customer" => self::getLogged()['id']
+          ]
+        ]
+      );
+
+      var_dump($customer); exit();
+
+      $db->insert_array([
+        "table" => "customers_uids",
+        "table_data" => [
+          "uid" => $_COOKIE["customer_uid"]
+        ]
+      ]);
     }
 
   }
