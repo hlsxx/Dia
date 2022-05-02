3972f78706c581551a1f200e7ed02069c89b715f
Bombarder70 <holespato@gmail.com>
2022-05-02 11:00:10 +0200

    Dia-get-structure improvements

--- Actions/dia_get_structure.php
+++ Actions/dia_get_structure.php
@@ -1,8 +1,16 @@
 <?php
 
+// GET tableName paramater
 $tableName = \Core\Controllers\WebController::getParam("tableName");
+
+// GET structureType parameter for tpye of structure to load
+$structureType = \Core\Controllers\WebController::getParam("structureType");
+
+// File path of .json file e.g.: Products.json
 $filePath = \Core\Controllers\WebController::getConfig()['dir']['web']
-	. "/Tables/".ucfirst($tableName).".json"
+	. "/Tables/" // default folder
+	.($structureType != "default" ? "{$structureType}/" : "") // If is custom type Tables/CustomType/Products.json
+	.ucfirst($tableName).".json"
 ;
 
 if (is_file($filePath)) {
