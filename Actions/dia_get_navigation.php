3aa93ba0b5b69d8bdb7205b4246491da8c944994
Bombarder70 <holespato@gmail.com>
2022-05-02 17:28:02 +0200

    get_navigation

---
+++ Actions/dia_get_navigation.php
@@ -0,0 +1,48 @@
+<?php
+
+global $db;
+
+$tableName = \Core\Controllers\WebController::getParam("tableName");
+
+/*$filePath = \Core\Controllers\WebController::getConfig()['dir']['web']
+	. "/Tables/Navigations/"
+	.ucfirst($tableName).".json"
+;
+
+if (is_file($filePath)) {
+	$tableStructure = json_decode(file_get_contents($filePath), TRUE);
+  $menuData = $tableStructure["menuData"];
+}*/
+
+$navbarList = $db->dbSelect(
+  tableName: $tableName,
+  conditions: [
+    "order_by" => "order_index ASC"
+  ]
+);
+
+foreach ($navbarList as $flatItem) {
+  if ($flatItem['id_parent'] == 0) {
+    $children = [];
+    foreach ($navbarList as $flatItemSub) {
+      if ($flatItemSub['id_parent'] == $flatItem['id']) {
+        $children[] = [
+          "name" => $flatItemSub['name'],
+          "link" => $flatItemSub['link'],
+          "icon" => $flatItemSub['icon'],
+        ];
+      }
+    }
+
+    $menuItems[] = [
+      "name" => $flatItem['name'],
+      "link" => $flatItem['link'],
+      "icon" => $flatItem['icon'],
+      "is_enabled" => $flatItem['is_enabled'],
+      "childrens" => $children,
+    ];
+  }
+}
+
+echo json_encode($menuItems);
+?>
